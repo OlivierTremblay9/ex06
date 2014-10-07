a70cbe4f442e9c2251ff21021f019309a00dcef6
OlivierTremblay9 <oli_0007@hotmail.com>
2014-10-07 11:02:23 -0400

    Final version

--- bin/ex06.dart
+++ bin/ex06.dart
@@ -1,6 +1,6 @@
 library define_functions;
 
-part 'fonction.dart';
+part '../Lib/fonction.dart';
 
 void ex06_no01() {
   var pal1 = 'Abc ded cba';
@@ -13,11 +13,11 @@ void ex06_no01() {
   pal3 = pal3.toLowerCase();
   pal4 = pal4.toLowerCase();
    
-  print('Number 1: \n Question: Determine if a given string is a palindrome.');
-  print('   Sentence 1: ${pal1} \n    Answer: ${palindrome(pal1)}');
-  print('   Sentence 2: ${pal2} \n    Answer: ${palindrome(pal2)}');
-  print('   Sentence 3: ${pal3} \n    Answer: ${palindrome(pal3)}');
-  print('   Sentence 4: ${pal4} \n    Answer: ${palindrome(pal4)}');
+  print('Number 1: \n Question: Determine if a given string is a palindrome.\n');
+  print('   Sentence 1: ${pal1} \n    Answer: ${palindrome(pal1)}\n');
+  print('   Sentence 2: ${pal2} \n    Answer: ${palindrome(pal2)}\n');
+  print('   Sentence 3: ${pal3} \n    Answer: ${palindrome(pal3)}\n');
+  print('   Sentence 4: ${pal4} \n    Answer: ${palindrome(pal4)}\n');
   print('\n******************************************************************');
 }
 
@@ -25,8 +25,8 @@ void ex06_no02() {
   DateTime a = new DateTime(2013, 01, 01); 
   DateTime b = new DateTime(2014, 01, 01); 
   int durationDif = Date(a, b);
-  print('Number 2: \n Question: Calculate the number of days between two dates');
-  print('   Date 1: ${a} \n   Date 2: ${b}');
+  print('Number 2: \n Question: Calculate the number of days between two dates.\n');
+  print('   Date 1: ${a} \n   Date 2: ${b}\n');
   print('   Answer in days: ${durationDif} ');
   print('\n******************************************************************');
 }
@@ -35,15 +35,15 @@ void ex06_no02() {
 void ex06_no03() {
   var num_result = 77.5;
   var calculate = convert(num_result);
-  print('Number 3: \n Question: Convert number grades to letter grades by using a standard');
-  print('   The note to convert is: ${num_result}%');
+  print('Number 3: \n Question: Convert number grades to letter grades by using a standard\n');
+  print('   The note to convert is: ${num_result}%\n');
   print('   The result is:${calculate}');
   print('\n******************************************************************');
 }
 
 
 void ex06_no04() { //This code is inpi
-  print('Number 4: \n Question: Given a list of names, make a list of three lists, first with names shorter than 8 letters,\n           second with names longer than 8 letters, and third with names that are 8 letters long.');
+  print('Number 4: \n Question: Given a list of names, make a list of three lists, first with names shorter than 8 letters,\n           second with names longer than 8 letters, and third with names that are 8 letters long.\n');
   var names = ['Olivier', 'Léa','Geneviève', 'Samuel','George', 'Andrews', 'William', 'Philippe', 'Jean-Dominic']; 
   List list = liste_Mane_list (names);  
   print('    Names are: $names');
@@ -52,7 +52,7 @@ void ex06_no04() { //This code is inpi
 }
 
 void ex06_no05() { //This program was inspired by: https://github.com/leduy10/ex06/blob/master/bin/functions.dart
-  print('Number 5: \n Question: Prepare two lists, one with the names of players and the other with the names of clubs. \n           Make the third list with clubs ordered by their names, where each club is followed by \n           its list of players.');
+  print('Number 5: \n Question: Prepare two lists, one with the names of players and the other with the names of clubs. \n           Make the third list with clubs ordered by their names, where each club is followed by \n           its list of players.|n');
   
   Map players_vs_team = {'Alex Galchenyuk #27' : 'Canadiens',  
                          'Brendan Gallagher #11' : 'Canadiens',  
@@ -67,7 +67,7 @@ void ex06_no05() { //This program was inspired by: https://github.com/leduy10/ex
                          'Tyler Ennis # 63' : 'Sabre', 
                          'Brian Flynn #65' : 'Sabre'};
  
-  print('\nListe of players:');
+  print('\nListe of players:\n');
   
   players_vs_team.forEach((k,v) => print('       $k'));
   
